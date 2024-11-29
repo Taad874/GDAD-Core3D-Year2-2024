e62c236caaa0651fe2ec2104d04c0590dc3cbd6a
Taad874 <Donnelly-A73@ulster.ac.uk>
2024-11-29 14:58:32 +0000

    Attempted to add a checkpoint array

--- Assets/A-MAZE-ING ROLL/Scripts/CheckPointSystem.cs
+++ Assets/A-MAZE-ING ROLL/Scripts/CheckPointSystem.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class CheckPointSystem : MonoBehaviour
 {
     [SerializeField] private GameObject[] checkPoints;
+    //[SerializeField] private LayerMask checkPointLay;
+    private int currentCheckPoint;
+
+    private void Awake()
+    {
+        checkPoints = GameObject.FindGameObjectsWithTag("CheckPoint");
+        currentCheckPoint = 0;
+        Debug.Log(checkPoints[currentCheckPoint].name);
+    }
     // Start is called before the first frame update
     void Start()
     {
